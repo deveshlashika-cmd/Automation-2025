ad1bff20f62060221c453b9b8cb18f422fb78bb2
Automation-2025 <deveshlashika@gmail.com>
2025-10-20 19:25:58 +0530

    first commit

---
+++ tests/sample.spec.js
@@ -0,0 +1,12 @@
+const {test, expect} = require("@playwright/test")
+
+test('Home',async ({page})=>{
+
+    await page.goto("https://playwright.dev/")
+
+      await expect(page).toHaveTitle(/Playwright/);
+
+      await page.pause();
+});
+
+
